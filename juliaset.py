ca3911e2397aed9c6b67e0e3c2fed6d840961d66
HobbitJack <jruteg@gmail.com>
2022-11-30 23:47:09 -0500

    Re-write using screen size
    NEEDS DOCSTRINGS

--- juliaset.py
+++ juliaset.py
@@ -4,9 +4,9 @@ It is recommended to have at least an 80 column display.
 # import math
 import imag
 
-# (x-range), (y-range), (resolution=(4, 4)), (divergent (depth), convergent, (0,0)) ("□", "■", "▣")
-PARAMETERS = ((-2, 0.5), (-1.12, 1.12), (4, 4), (" ", "*", "@"))  # Mandelbrot
-# PARAMETERS = ((-2, 2), (-2, 2), (4, 4), ((" ", ",", ".", "'", ":"), "*", "@"))  # Julia sets
+# (x-range), (y-range), (res=(80, 40)), (divergent (depth), convergent, (0,0)) ("□", "■", "▣")
+PARAMETERS = ((-2, 0.5), (-1.12, 1.12), (80, 40), (" ", "*", "@"))  # Mandelbrot
+# PARAMETERS = ((-2, 2), (-2, 2), (160, 80), ((" ", ",", ".", "'", ":"), "*", "@"))  # Julia sets
 
 
 def algorithm(z: imag.imag, c: imag.imag) -> imag.imag:
@@ -25,71 +25,63 @@ def algorithm(z: imag.imag, c: imag.imag) -> imag.imag:
     return z
 
 
+def test_point(point: imag.imag) -> int:
+    if len(PARAMETERS[3]) == 3 and point.a == 0 and point.b == 0:
+        return -1
+    z = imag.imag(0, 0)
+    for run in range(100):
+        z = algorithm(z, point)
+        if z.a < -2 or z.a > 2 or z.b < -2 or z.b > 2:
+            return run
+    return 100
+
+
+def point_char_to_draw(runs: int) -> str:
+    if runs == -1:
+        return PARAMETERS[3][2]
+
+    if runs == 100:
+        return PARAMETERS[3][1]
+
+    count = 0
+    while (100 / len(PARAMETERS[3][0])) * (count + 1) < runs:
+        count += 1
+    return PARAMETERS[3][0][count]
+
+
 def main():
     """This function implements the above in order to actually graph onto a CLI."""
     print()
 
-    # Generate line-wrap length
-    line_wrap = len(
-        [
-            ""
-            for _ in range(
-                int(PARAMETERS[0][0] * 100),
-                int(PARAMETERS[0][1] * 100),
-                PARAMETERS[2][1],
+    print(" ", "_" * PARAMETERS[2][0], sep="", end="\n")
+    for y in range(PARAMETERS[2][1]):
+        print("|", end="")
+        for x in range(PARAMETERS[2][0]):
+            print(
+                point_char_to_draw(
+                    test_point(
+                        imag.imag(
+                            PARAMETERS[0][0]
+                            + x
+                            * (
+                                (PARAMETERS[0][1] - PARAMETERS[0][0]) / PARAMETERS[2][0]
+                            ),
+                            PARAMETERS[1][1]
+                            - y
+                            * (
+                                (PARAMETERS[1][1] - PARAMETERS[1][0]) / PARAMETERS[2][1]
+                            ),
+                        )
+                    )
+                ),
+                end="",
             )
-        ]
-    )
 
-    # Generate points
-    points = []
-    for b in range(
-        int(PARAMETERS[1][1] * 100), int(PARAMETERS[1][0] * 100), -PARAMETERS[2][0]
-    ):
-        for a in range(
-            int(PARAMETERS[0][0] * 100), int(PARAMETERS[0][1] * 100), PARAMETERS[2][1]
-        ):
-            points.append(imag.imag(a / 100, b / 100))
-
-    count = 0
-    print(" ", "_" * line_wrap, sep="", end="\n|")
-    for point in points:
-        count += 1
-
-        # Print (0, 0)
-        if point.a == 0 and point.b == 0:
-            print(PARAMETERS[3][2], end="")
-            continue
-
-        # Run algorithm
-        z = imag.imag(0, 0)
-        for run in range(100):
-            z = algorithm(z, point)
-            if z.a > 2 or z.a < -2 or z.b > 2 or z.b < -2:
-                if not isinstance(PARAMETERS[3][0], tuple):
-                    print(PARAMETERS[3][0], end="")
-                    break
-                else:
-
-                    num_sep = 100 // len(PARAMETERS[3][0])
-                    for index, char in enumerate(PARAMETERS[3][0]):
-                        if run < int(num_sep) * (int(index) + 1):
-                            print(char, end="")
-                            break
-                    break
-
+        if y != PARAMETERS[2][1] - 1:
+            print("|")
         else:
-            print(PARAMETERS[3][1], end="")
-
-        # Line wrap
-        if count % line_wrap == 0:
-            print("|\n", end="")
-
-        if count % line_wrap == 0:
-            if point != points[-1]:
-                print("|", sep="", end="")
-
-    print(" ", "‾" * line_wrap, sep="")
+            print("|\n ", "‾" * PARAMETERS[2][0], sep="", end="")
+    print(" ")
 
 
 if __name__ == "__main__":
